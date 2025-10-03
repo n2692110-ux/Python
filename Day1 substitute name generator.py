cf5c606478cd7e236df16c2e1411773e983e1369
Nandini <n2692110@gmail.com>
2025-10-03 16:54:06 +0530

    Added new file

---
+++ Day1 substitute name generator.py
@@ -0,0 +1,13 @@
+print("Welcome to SUBSTITUTE NAME GENERATOR program")
+
+flower = input("Your favourite flower? ")
+print("Hello " + flower)
+
+city = input("What is your city? ")
+print("Welcome in " + city)
+
+substitute_name = (flower + " "+ city)
+print("Your SUBSTITUTE NAME is : " + substitute_name)
+
+length = len(substitute_name)
+print("Length of your SUBSTITUTE NAME is " + str(length))
